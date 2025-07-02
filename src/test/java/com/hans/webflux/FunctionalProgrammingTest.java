1c5294e6b5cd6bbd5e69b604dc1046079c50a94a
DaeHan Kim <kjs50458281@gmail.com>
2025-07-02 23:28:42 +0900

    기능 프로그래밍 테스트에 IntStream과 Flux를 사용한 새로운 테스트 케이스 추가: 1부터 9까지의 정수를 생성하고 변환 및 필터링하는 예제 포함.

--- src/test/java/com/hans/webflux/FunctionalProgrammingTest.java
+++ src/test/java/com/hans/webflux/FunctionalProgrammingTest.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 @SpringBootTest
 public class FunctionalProgrammingTest {
@@ -22,6 +24,36 @@ public class FunctionalProgrammingTest {
     sink = filter(sink, (data) -> data % 4 == 0);
 
     forEach(sink, (data) -> System.out.println(data));
+  }
+
+  @Test
+  public void produceOneToNineStream() {
+    IntStream.rangeClosed(1, 9).boxed()
+        .map((data) -> data * 4)
+        .filter((data) -> data % 4 == 0)
+        .forEach(System.out::println); // 최종 연산이 있어야 실행이됨(collect,foreach, min, max)
+  }
+
+  @Test
+  public void onenineListFlux() {
+    Flux<Integer> intFlux = Flux.create(fluxSink -> {
+      for (int i = 1; i <= 9; i++) {
+        fluxSink.next(i);
+      }
+      fluxSink.complete();
+    });
+
+    // 위에 Fulx는 함수덩어리라서 subscribe로 구독을 해줘야 실행이됨
+    intFlux.subscribe(data -> System.out.println("WebFlux가 구독중!! " + data));
+    System.out.println("Netty 이벤트 루프로 스레드 복귀 !!");
+  }
+
+  @Test
+  public void produceOneToNineFluxOperator() {
+    Flux.fromIterable(IntStream.rangeClosed(1, 9).boxed().toList())
+        .map((data) -> data * 4)  // operator 대부분이 stream과 유사하게 동작
+        .filter((data) -> data % 4 == 0)
+        .subscribe(System.out::println);
   }
 
   private void forEach(List<Integer> sink, Consumer<Integer> consumer) {
